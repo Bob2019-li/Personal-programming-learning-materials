cf9637305c8f5a35cf396f9e3382755b0ad63ae7
lizequan <2549524215@qq.com>
2022-01-16 18:55:01 +0800

    更新

--- 积累/wmctrl-main.c
+++ 积累/wmctrl-main.c
@@ -1,30 +1,4 @@
 
-/* license {{{ */
-/* 
-
-wmctrl
-A command line tool to interact with an EWMH/NetWM compatible X Window Manager.
-
-Author, current maintainer: Tomas Styblo <tripie@cpan.org>
-
-Copyright (C) 2003
-
-This program is free software which I release under the GNU General Public
-License. You may redistribute and/or modify this program under the terms
-of that license as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-To get a copy of the GNU General Puplic License,  write to the
-Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-*/
-/* }}} */
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
